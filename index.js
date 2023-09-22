63a69d488b3842d7684befe0814b3e42d9ee71db
bolajeee <ibrahimomobolaji1999@gmail.com>
2023-09-22 15:31:53 +0100

    edit

--- index.js
+++ index.js
@@ -1,60 +1,44 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
+const fs = require('fs');
+const util = require('util');
+const chalk = require('chalk')
+const path = require('path')
 
- // wrapping lstat as promise #2
-// const util = require('util')
-// const stat = util.promisify(fs.lstat)
+// Method #2
+// const lstat = util.promisify(fs.lstat);
 
- // wrapping lstat as promise #3
- const { lstat } = fs.promises
-//  or
-//  const lstat = fs.promises(lstat)
+// Method #3
+const { lstat } = fs.promises;
 
-fs.readdir(process.cwd(), async (err, filenames) => {
-    if(err){
-        throw new error(err)
-    }
+const targetDir = process.argv[2] || process.cwd()
+fs.readdir(targetDir, async (err, filenames) => {
+  if (err) {
+    console.log(err);
+  }
 
-   const statPromises = filenames.map(filename => {
-    return lstat(filename)
-   })
+  const statPromises = filenames.map(filename => {
+    return lstat(path.join(targetDir, filename));
+  });
 
-   const allstats = await Promise.all(statPromises)
+  const allStats = await Promise.all(statPromises);
 
-   for(let stats of allstats){
-    const index = allstats.indexOf(stats)
+  for (let stats of allStats) {
+    const index = allStats.indexOf(stats);
 
-    console.log(filenames[index], stats.isFile())
-   }
-    
+    console.log(filenames[index], stats.isFile());
+  }
+});
 
-    // wrapping lstat as promise #1
-    // const lstat = filename => {
-    //     return new Promise ((resolve, reject) => {
-    //         fs.lstat(filename, (err, stats)){
-    //         if(err){
-    //             reject(err)
-    //         }
-    //         resolve(stats)
-    //     }
-    //     })
-    // }
+// Method #1
+// const lstat = filename => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stats) => {
+//       if (err) {
+//         reject(err);
+//       }
 
-
-
-    // bad code
-    // for(let file of filename){
-    //     fs.lstat(file,(err, filetype) => {
-    //         if(err){
-    //             throw new error(err)
-    //         }
-
-    //         console.log(file, filetype.isFile())
-    //     })
-    // }
-    // bad code
-
-
-})
-
+//       resolve(stats);
+//     });
+//   });
+// };
